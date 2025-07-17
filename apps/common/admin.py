157471b5d4ad3d627369d25cbcf00c66cd4e36b3
Jaloliddin <jmamatmusayev@gmail.com>
2025-07-17 17:20:29 +0500

    Refactor user and translation models, remove deprecated VersionHistory, and update admin configurations

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
-from . import models
-
-
-@admin.register(models.VersionHistory)
-class VersionHistoryAdmin(TranslationAdmin):
-    list_display = ("id", "version", "required", "created_at", "updated_at")
-    list_display_links = ("id", "version")
-    list_filter = ("required", "created_at", "updated_at")
-    search_fields = ("version",)
+from apps.common import models
 
 
 @admin.register(models.FrontendTranslation)
@@ -17,16 +9,14 @@ class FrontTranslationAdmin(TranslationAdmin):
     list_display_links = ("id", "key")
     list_filter = ("created_at", "updated_at")
     search_fields = ("key", "text")
-    
-    # TranslationAdmin avtomatik ravishda barcha til maydonlarini ko'rsatadi
-    # Qo'shimcha fieldsets kerak emas
-    
-    class Media:
-        js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
-        )
-        css = {
-            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }
+    readonly_fields = ("created_at", "updated_at", "created_by", "updated_by")
+
+    # class Media:
+    #     js = (
+    #         'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+    #         'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+    #         'modeltranslation/js/tabbed_translation_fields.js',
+    #     )
+    #     css = {
+    #         'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+    #     }
--- apps/common/translation.py
+++ apps/common/translation.py
@@ -1,14 +1,9 @@
 # translation.py
 from modeltranslation.translator import TranslationOptions, register
 
-from . import models
+from apps.common import models
 
 
 @register(models.FrontendTranslation)
 class FrontTranslationOptions(TranslationOptions):
     fields = ("text",)
-
-
-@register(models.VersionHistory)
-class VersionHistoryTranslationOptions(TranslationOptions):
-    fields = ("version",)
