cf1c1a1283e3670dadd6120a0f6ed31242ab58d7
Jaloliddin <jmamatmusayev@gmail.com>
2025-08-06 22:46:06 +0500

    add initial migration and update models with verbose names for better localization

--- apps/structure/models/employees.py
+++ apps/structure/models/employees.py
@@ -11,44 +11,44 @@ class UserInfo(TimeStamped, Authored):
     OTHER = 'other'
 
     CITIZENSHIP = (
-        (NONE, 'None'),
-        ('russian', 'Russian'),
-        ('kazakh', 'Kazakh'),
-        ('karakalpak', 'Karakalpak'),
-        ('tajik', 'Tajik'),
-        ('turkmen', 'Turkmen'),
-        ('kyrgyz', 'Kyrgyz'),
-        ('ukrainian', 'Ukrainian'),
-        ('belarusian', 'Belarusian'),
-        ('armenian', 'Armenian'),
-        ('korean', 'Korean'),
-        ('afghan', 'Afghan'),
-        (UZBEK, 'O\'zbek'),
-        (OTHER, 'Boshqa')
+        (NONE, _('None')),
+        ('russian', _('Russian')),
+        ('kazakh', _('Kazakh')),
+        ('karakalpak', _('Karakalpak')),
+        ('tajik', _('Tajik')),
+        ('turkmen', _('Turkmen')),
+        ('kyrgyz', _('Kyrgyz')),
+        ('ukrainian', _('Ukrainian')),
+        ('belarusian', _('Belarusian')),
+        ('armenian', _('Armenian')),
+        ('korean', _('Korean')),
+        ('afghan', _('Afghan')),
+        (UZBEK, _("O'zbek")),
+        (OTHER, _('Boshqa'))
     )
 
     MALE = 'male'
     FEMALE = 'female'
 
     GENDER_CHOISES = (
-        (MALE, 'Erkak'),
-        (FEMALE, 'Ayol')
+        (MALE, _('Erkak')),
+        (FEMALE, _('Ayol'))
     )
 
-    full_name = models.CharField(max_length=255)
-    xmn_id = models.CharField(max_length=30)
-    photo = models.ImageField(upload_to="users/", null=True, blank=True)
-    image = models.URLField(null=True,blank=True)
-    gender = models.CharField(max_length=10, choices=GENDER_CHOISES)
-    year_of_enter = models.CharField(max_length=4, null=True, blank=True)
-    birthday = models.DateField(null=True, blank=True)
-    email = models.EmailField(null=True, blank=True)
-    phone = models.CharField(max_length=20, null=True, blank=True)
-    passport = models.CharField(max_length=255, null=True, blank=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
+    full_name = models.CharField(_("To'liq ism"), max_length=255)
+    xmn_id = models.CharField(_("XMN ID"), max_length=30)
+    photo = models.ImageField(_("Foydalanuvchi rasmi"), upload_to="users/", null=True, blank=True)
+    image = models.URLField(_("Rasm URL manzili"), null=True, blank=True)
+    gender = models.CharField(_("Jinsi"), max_length=10, choices=GENDER_CHOISES)
+    year_of_enter = models.CharField(_("Kirish yili"), max_length=4, null=True, blank=True)
+    birthday = models.DateField(_("Tug'ilgan sana"), null=True, blank=True)
+    email = models.EmailField(_("Elektron pochta"), null=True, blank=True)
+    phone = models.CharField(_("Telefon raqami"), max_length=20, null=True, blank=True)
+    passport = models.CharField(_("Pasport ma'lumotlari"), max_length=255, null=True, blank=True)
+    address = models.CharField(_("Manzil"), max_length=255, blank=True, null=True)
     citizenship = models.CharField(
-        max_length=255, choices=CITIZENSHIP, default=UZBEK)
-    age = models.IntegerField(default=18)
+        _("Fuqarolik"), max_length=255, choices=CITIZENSHIP, default=UZBEK)
+    age = models.IntegerField(_("Yosh"), default=18)
 
     @property
     def ages(self):
@@ -84,10 +84,10 @@ class Employee(UserInfo):
     PROFESSOR = "13"
 
     ACADEMIC_TITLE = (
-        (UNTITLED, "Untitled"),
-        (DOTSENT, "Dotsent"),
-        (SENIOR_LECTURER, "Senior Lecturer"),
-        (PROFESSOR, "Professor"),
+        (UNTITLED, _("Untitled")),
+        (DOTSENT, _("Dotsent")),
+        (SENIOR_LECTURER, _("Senior Lecturer")),
+        (PROFESSOR, _("Professor")),
     )
 
     FULL = "11"
@@ -97,11 +97,11 @@ class Employee(UserInfo):
     INTERNAL_MAIN = "15"
 
     WORK_TYPE_SELECT = (
-        (FULL, "Asosiy ish joy"),
-        (EXTERNAL, "O'rindoshlik (tashqi)"),
-        (HOURLY, "Soatbay"),
-        (INTERNAL_ADJUNCT, "O'rindoshlik (ichki-qo'shimcha)"),
-        (INTERNAL_MAIN, "O'rindoshlik (ichki-asosiy)"),
+        (FULL, _("Asosiy ish joy")),
+        (EXTERNAL, _("O'rindoshlik (tashqi)")),
+        (HOURLY, _("Soatbay")),
+        (INTERNAL_ADJUNCT, _("O'rindoshlik (ichki-qo'shimcha)")),
+        (INTERNAL_MAIN, _("O'rindoshlik (ichki-asosiy)")),
     )
 
     UNDEGREE = "10"
@@ -109,9 +109,9 @@ class Employee(UserInfo):
     DOCTOR_OF_SCIENCES = "12"
 
     ACADEMIC_DEGREE = (
-        (UNDEGREE, "Darajasiz"),
-        (CANDIDATE_OF_SCIENCES, "Fan nomzodi (PhD)"),
-        (DOCTOR_OF_SCIENCES, "Fan doktori (DSc)"),
+        (UNDEGREE, _("Darajasiz")),
+        (CANDIDATE_OF_SCIENCES, _("Fan nomzodi (PhD)")),
+        (DOCTOR_OF_SCIENCES, _("Fan doktori (DSc)")),
     )
 
     STAJYOR = "11"
@@ -127,42 +127,40 @@ class Employee(UserInfo):
     OTHER = "other"
 
     POSITION_SELECT = (
-        # TEACHERS = "O'qituvchilar",
-        (STAJYOR, "Stajor O'qituvchi"),
-        (ASISTENT, "Asistent"),
-        (BIG_TEACHER, "Katta o'qituvchi"),
-        (DOTSENT, "Dotsent"),
-        (PROFESSOR, "Professor"),
-        # OTHER = "Boshqa",
-        (DEPARTMENT_USER, 'Kafedra mudiri'),
-        (DEKAN, "Dekan"),
-        (DEKAN_ASSISTANT, "Dekan muovini"),
-        (TUTOR, "Tyutor"),
-        (ENGINEER, "1-toifali muhandis"),
-        (OTHER, "Boshqa")
+        (STAJYOR, _("Stajor O'qituvchi")),
+        (ASISTENT, _("Asistent")),
+        (BIG_TEACHER, _("Katta o'qituvchi")),
+        (DOTSENT, _("Dotsent")),
+        (PROFESSOR, _("Professor")),
+        (DEPARTMENT_USER, _('Kafedra mudiri')),
+        (DEKAN, _("Dekan")),
+        (DEKAN_ASSISTANT, _("Dekan muovini")),
+        (TUTOR, _("Tyutor")),
+        (ENGINEER, _("1-toifali muhandis")),
+        (OTHER, _("Boshqa"))
     )
 
     EMPLOYEE_TYPE = (
-        ("other", "Boshqa"),
-        ("teacher", "Professor-o‘qituvchi xodim"),
-        ("dekan", "Administrativ-boshqaruv xodim"),
-        ("tutor", "O‘quv-yordamchi va texnik xodim")
+        ("other", _("Boshqa")),
+        ("teacher", _("Professor-o‘qituvchi xodim")),
+        ("dekan", _("Administrativ-boshqaruv xodim")),
+        ("tutor", _("O‘quv-yordamchi va texnik xodim"))
     )
 
-    employee_id_number = models.CharField(max_length=20)
-    meta_id = models.CharField(max_length=20)
-    uzkadr_id = models.CharField(max_length=10)
-    specialty = models.CharField(max_length=255, null=True, blank=True)
+    employee_id_number = models.CharField(_("Xodim ID raqami"), max_length=20)
+    meta_id = models.CharField(_("Meta ID"), max_length=20)
+    uzkadr_id = models.CharField(_("UZKADR ID"), max_length=10)
+    specialty = models.CharField(_("Mutaxassisligi"), max_length=255, null=True, blank=True)
     academicDegree = models.CharField(
-        max_length=255, choices=ACADEMIC_DEGREE, default=UNDEGREE)
+        _("Ilmiy darajasi"), max_length=255, choices=ACADEMIC_DEGREE, default=UNDEGREE)
     academicRank = models.CharField(
-        max_length=255, choices=ACADEMIC_TITLE, default=UNTITLED)
+        _("Ilmiy unvoni"), max_length=255, choices=ACADEMIC_TITLE, default=UNTITLED)
     employmentForm = models.CharField(
-        max_length=255, choices=WORK_TYPE_SELECT, default=FULL)
-    staffPosition = models.CharField(max_length=255, choices=POSITION_SELECT)
+        _("Ish shakli"), max_length=255, choices=WORK_TYPE_SELECT, default=FULL)
+    staffPosition = models.CharField(_("Lavozimi"), max_length=255, choices=POSITION_SELECT)
     employeeType = models.CharField(
-        max_length=255, choices=EMPLOYEE_TYPE, default="teacher")
-    is_foreign = models.BooleanField(default=False)
+        _("Xodim turi"), max_length=255, choices=EMPLOYEE_TYPE, default="teacher")
+    is_foreign = models.BooleanField(_("Chet el fuqarosi"), default=False)
 
     def __str__(self):
         return f"{self.full_name} - {self.staffPosition}"
@@ -173,4 +171,5 @@ class Employee(UserInfo):
 
     class Meta:
         db_table = 'employee'
-
+        verbose_name = _("Xodim")
+        verbose_name_plural = _("Xodimlar")
--- apps/structure/models/university.py
+++ apps/structure/models/university.py
@@ -14,98 +14,96 @@ from apps.structure.models.employees import Employee
 
 #     def __str__(self):
 #         return self.name
-    
 
 class Faculty(TimeStamped, Authored):
     '''Fakultet'''
     COLOR_PALETTE = [
-            ("#FFFFFF", "white", ),
-            ("#000000", "black", ),
-            ("#0000FF", "blue",),
-            ("#00FF00", "green",),
-            ("#FF0000", "red",),
+        ("#FFFFFF", "white", ),
+        ("#000000", "black", ),
+        ("#0000FF", "blue",),
+        ("#00FF00", "green",),
+        ("#FF0000", "red",),
     ]
 
-    name = models.CharField(max_length=255)
-    xmn_id = models.CharField(max_length=20)
-    code = models.CharField(max_length=20)
-    description = models.TextField(blank=True, null=True)
-    banner = models.ImageField(upload_to="faculty/banner", blank=True, null=True)
-    icon = models.ImageField(upload_to="faculty/icon", blank=True, null=True)
+    name = models.CharField(max_length=255, verbose_name=_("Fakultet nomi"))
+    xmn_id = models.CharField(max_length=20, verbose_name=_("XMN ID"))
+    code = models.CharField(max_length=20, verbose_name=_("Kod"))
+    description = models.TextField(blank=True, null=True, verbose_name=_("Tavsif"))
+    banner = models.ImageField(upload_to="faculty/banner", blank=True, null=True, verbose_name=_("Banner"))
+    icon = models.ImageField(upload_to="faculty/icon", blank=True, null=True, verbose_name=_("Ikon"))
     # color = ColorField(samples=COLOR_PALETTE)
-    position = models.PositiveSmallIntegerField(default=0)
-
+    position = models.PositiveSmallIntegerField(default=0, verbose_name=_("Pozitsiya"))
 
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = 'faculty'
-        verbose_name = _("Faculty")
-        verbose_name_plural = _("Faculties")
+        verbose_name = _("Fakultet")
+        verbose_name_plural = _("Fakultetlar")
 
-    
+
 class Department(TimeStamped, Authored):
     '''Kafedra'''
-    name = models.CharField(max_length=255)
-    xmn_id = models.CharField(max_length=10)
-    code = models.CharField(max_length=20)
-    faculty = models.ForeignKey(Faculty, on_delete=models.RESTRICT, related_name="departments")
-    description = models.TextField(blank=True, null=True)
-    position = models.PositiveSmallIntegerField(default=0)
+    name = models.CharField(max_length=255, verbose_name=_("Kafedra nomi"))
+    xmn_id = models.CharField(max_length=10, verbose_name=_("XMN ID"))
+    code = models.CharField(max_length=20, verbose_name=_("Kod"))
+    faculty = models.ForeignKey(Faculty, on_delete=models.RESTRICT, related_name="departments", verbose_name=_("Fakultet"))
+    description = models.TextField(blank=True, null=True, verbose_name=_("Tavsif"))
+    position = models.PositiveSmallIntegerField(default=0, verbose_name=_("Pozitsiya"))
 
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = 'department'
-        verbose_name = _("Department")
-        verbose_name_plural = _("Departments")
+        verbose_name = _("Kafedra")
+        verbose_name_plural = _("Kafedralar")
 
 
 class Specialty(TimeStamped, Authored):
     '''Yunalish'''
     EDUCATION_TYPE = (
-        (11, "Bakalavr"),
-        (12, "Magistr"),
-        (13, "Boshqa"),
-        (14, "Doktorantura PhD"),
+        (11, _("Bakalavr")),
+        (12, _("Magistr")),
+        (13, _("Boshqa")),
+        (14, _("Doktorantura PhD")),
     )
     LOCALITY_TYPE = (
-        (10, 'Boshqa'),
-        (11, "Mahalliy"),
-        (12, "Qo‘shma"),
-        (13, "Bo'lim"),
+        (10, _('Boshqa')),
+        (11, _("Mahalliy")),
+        (12, _("Qo‘shma")),
+        (13, _("Bo'lim")),
     )
-    name = models.CharField(max_length=255)
-    xmn_id = models.CharField(max_length=10)
-    code = models.CharField(max_length=15)
-    faculty = models.ForeignKey(Faculty, on_delete=models.RESTRICT, related_name="specialities")
-    department = models.ForeignKey(Department, on_delete=models.SET_NULL, related_name="specialities", null=True)
-    educationType = models.IntegerField(choices=EDUCATION_TYPE, default=11)
-    localityType = models.IntegerField(choices=LOCALITY_TYPE, default=11)
-    description = models.TextField(blank=True, null=True)
-    position = models.PositiveSmallIntegerField(default=0)
+    name = models.CharField(max_length=255, verbose_name=_("Yo'nalish nomi"))
+    xmn_id = models.CharField(max_length=10, verbose_name=_("XMN ID"))
+    code = models.CharField(max_length=15, verbose_name=_("Kod"))
+    faculty = models.ForeignKey(Faculty, on_delete=models.RESTRICT, related_name="specialities", verbose_name=_("Fakultet"))
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, related_name="specialities", null=True, verbose_name=_("Kafedra"))
+    educationType = models.IntegerField(choices=EDUCATION_TYPE, default=11, verbose_name=_("Ta'lim turi"))
+    localityType = models.IntegerField(choices=LOCALITY_TYPE, default=11, verbose_name=_("Mahalliylik turi"))
+    description = models.TextField(blank=True, null=True, verbose_name=_("Tavsif"))
+    position = models.PositiveSmallIntegerField(default=0, verbose_name=_("Pozitsiya"))
 
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = 'specialty'
-        verbose_name = _("Specialty")
-        verbose_name_plural = _("Specialties")
+        verbose_name = _("Yo'nalish")
+        verbose_name_plural = _("Yo'nalishlar")
 
 
 class FacultyEmployee(TimeStamped, Authored):
     '''Fakultet xodimlari'''
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name="employees")
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="faculty_employees")
-    position = models.CharField(max_length=255, choices=Employee.POSITION_SELECT, default=Employee.OTHER)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, related_name="employees", verbose_name=_("Fakultet"))
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="faculty_employees", verbose_name=_("Xodim"))
+    position = models.CharField(max_length=255, choices=Employee.POSITION_SELECT, default=Employee.OTHER, verbose_name=_("Lavozim"))
 
     class Meta:
         db_table = 'faculty_employee'
-        verbose_name = _("Faculty Employee")
-        verbose_name_plural = _("Faculty Employees")
+        verbose_name = _("Fakultet xodimi")
+        verbose_name_plural = _("Fakultet xodimlari")
 
     def __str__(self):
         return f"{self.id}"
@@ -113,14 +111,14 @@ class FacultyEmployee(TimeStamped, Authored):
 
 class DepartmentEmployee(TimeStamped, Authored):
     '''Kafedra xodimlari'''
-    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="employees")
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="department_employees")
-    position = models.CharField(max_length=255, choices=Employee.POSITION_SELECT, default=Employee.OTHER)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="employees", verbose_name=_("Kafedra"))
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="department_employees", verbose_name=_("Xodim"))
+    position = models.CharField(max_length=255, choices=Employee.POSITION_SELECT, default=Employee.OTHER, verbose_name=_("Lavozim"))
 
     class Meta:
         db_table = 'department_employee'
-        verbose_name = _("Department Employee")
-        verbose_name_plural = _("Department Employees")
+        verbose_name = _("Kafedra xodimi")
+        verbose_name_plural = _("Kafedra xodimlari")
 
     def __str__(self):
         return f"{self.id}"
