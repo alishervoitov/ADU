729e71a99f680edbe66d4861861d264a7ea4eb89
Jaloliddin <jmamatmusayev@gmail.com>
2025-08-07 02:10:08 +0500

    Fix URL path for HomePageTextListView and update comment for clarity

--- apps/api_endpoints/structure/urls.py
+++ apps/api_endpoints/structure/urls.py
@@ -10,7 +10,7 @@ from . import (
 app_name = "structure"
 
 urlpatterns = [
-    path("home", HomePageTextListView.as_view(), name="home_page_texts"),
-    path("HomePage", HomePageTextFilterListAPIView.as_view(), name="home_page_texts_filter"),
+    path("HomePage", HomePageTextListView.as_view(), name="home_page_texts"),
+    # path("HomePage", HomePageTextFilterListAPIView.as_view(), name="home_page_texts_filter"),
     path("UniversityBaseInfo", UniversityBaseInfoView.as_view(), name="university_base_info"),
 ]
