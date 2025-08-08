6af7fd322fd45e5ff440aa156c3cb7b2fa59206f
baxodir9 <bahodiramondullayev@gmail.com>
2025-08-08 16:35:41 +0500

    news retrieve

--- apps/api_endpoints/media_manage/urls.py
+++ apps/api_endpoints/media_manage/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path, include
-from .views import NewsListView, NewTypeListView
+from .views import *
 
 
 app_name = "media_manage"
 
 urlpatterns = [
     path("news/", NewsListView.as_view(), name="news-list"),
+    path("news/<int:pk>/", NewsRetrieveView.as_view(), name="news-detail"),
     path("newstype/", NewTypeListView.as_view(), name="newstype-list"),
 ]
