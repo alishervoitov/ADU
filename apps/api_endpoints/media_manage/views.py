80325bf4b719b5fbcf86734b120eb33a9080b8ff
baxodir9 <bahodiramondullayev@gmail.com>
2025-08-11 15:55:58 +0500

    doc model api s create

--- apps/api_endpoints/media_manage/views.py
+++ apps/api_endpoints/media_manage/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from apps.media_manage.models import News, NewType
+from apps.media_manage.models import News, NewType, DocumentType, Documents
 from . import serializers
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
@@ -35,7 +35,6 @@ class NewsListView(ListAPIView):
                 openapi.IN_QUERY,
                 description='Filter by type of News. Available types: main, global, academic, history',
                 type=openapi.TYPE_STRING,
-                enum=['short',],
                 required=False
             )
         ],
@@ -54,6 +53,55 @@ class NewsRetrieveView(APIView):
         try:
             news = News.objects.get(pk=pk)
             serializer = serializers.NewsSerializer(news)
+            news.viewed_count += 1
+            news.save()
             return Response(serializer.data)
         except News.DoesNotExist:
             return Response({"detail": "News not found"}, status=404)
+
+
+class DocumentTypeListView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        document_types = DocumentType.objects.all()
+        serializer = serializers.DocumentTypeSerializer(document_types, many=True)
+        return Response(serializer.data)
+
+
+class DocumentsListView(ListAPIView):
+    permission_classes = [AllowAny]
+    queryset = Documents.objects.all()
+    serializer_class = serializers.DocumentsSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['title', 'type__name']
+    filterset_fields = ['type']
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'type',
+                openapi.IN_QUERY,
+                description='Filter by type of Document. Available types: main, global, academic, history',
+                type=openapi.TYPE_STRING,
+                required=False
+            )
+        ],
+        responses={200: serializers.DocumentsSerializer(many=True)},
+        operation_summary="List Documents with filtering",
+        operation_description="Get a list of Documents objects with optional filtering by type"
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+
+class DocumentsRetrieveView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, slug, *args, **kwargs):
+        try:
+            document = Documents.objects.get(slug=slug)
+            serializer = serializers.DocumentsSerializer(document)
+            return Response(serializer.data)
+        except Documents.DoesNotExist:
+            return Response({"detail": "Document not found"}, status=404)
