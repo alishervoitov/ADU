85a4f0204c5834e03101ab4c247fe1446921f55c
Jaloliddin Mamatmusayev <jmamatmusayev@gmail.com>
2025-10-28 10:25:59 +0500

    add department user tasks for serializer

--- apps/api_endpoints/structure/Department/serializers.py
+++ apps/api_endpoints/structure/Department/serializers.py
@@ -1,9 +1,33 @@
 from rest_framework import serializers
 from apps.structure.models import Employee, Department
-from apps.api_endpoints.structure.Faculty.serializers import EmployeeSerializer
+# from apps.api_endpoints.structure.Faculty.serializers import EmployeeSerializer
 from django.db.models import Q
 
-
+class EmployeeSerializer(serializers.ModelSerializer):
+    photo = serializers.SerializerMethodField()
+    task = serializers.SerializerMethodField()
+    
+    def get_photo(self, obj):
+        if obj.photo:
+            request = self.context.get('request')
+            if request:
+                return request.build_absolute_uri(obj.photo.url)
+            return obj.photo.url
+        return None
+    
+    def get_task(self, obj):
+        if self.task:
+            return self.task
+        return obj.task
+    
+    class Meta:
+        model = Employee
+        fields = (
+            'id', 'full_name', 'employeeType', 
+            'staffPosition', 'academicRank', 'academicDegree',
+            'specialty', 'photo', 'email', 'phone', 'admission_dates', 
+            'admission_time', 'employeeRank', 'task'
+        )
 
 
 class DepartmentDetailSerializer(serializers.ModelSerializer):
@@ -11,10 +35,10 @@ class DepartmentDetailSerializer(serializers.ModelSerializer):
     employees = serializers.SerializerMethodField()
 
     def get_decan(self, obj):
-        return EmployeeSerializer(obj.employees.filter(position=Employee.DEPARTMENT_USER).first().employee, context=self.context).data if obj.employees.filter(position=Employee.DEPARTMENT_USER).exists() else None
+        return EmployeeSerializer(obj.employees.filter(position=Employee.DEPARTMENT_USER).first().employee, context=self.context, task=obj.employees.filter(position=Employee.DEPARTMENT_USER).first().task).data if obj.employees.filter(position=Employee.DEPARTMENT_USER).exists() else None
 
     def get_employees(self, obj):
-        return [EmployeeSerializer(emp.employee, context=self.context).data for emp in obj.employees.filter(~Q(position=Employee.DEPARTMENT_USER))] if obj.employees.exists() else []
+        return [EmployeeSerializer(emp.employee, context=self.context, task=emp.task).data for emp in obj.employees.filter(~Q(position=Employee.DEPARTMENT_USER))] if obj.employees.exists() else []
 
     class Meta:
         model = Department
