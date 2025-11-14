6a96916b10a4888294d0f210d42dc82829cfda13
Jaloliddin <jmamatmusayev@gmail.com>
2025-11-14 15:09:45 +0500

    fix: remove trailing slashes from FAQ and news type URLs

--- apps/api_endpoints/blog/urls.py
+++ apps/api_endpoints/blog/urls.py
@@ -12,7 +12,7 @@ app_name = "blog"
 
 urlpatterns = [
     path("interactive-services/", InteractiveServiceListView.as_view(), name="interactive_service_list"),
-    path("faqs/", FAQListView.as_view(), name="faq_list"),
+    path("faqs", FAQListView.as_view(), name="faq_list"),
     path("contact", ContactCreateView.as_view(), name="contact_create"),
     path("GlobalSearch/", SearchServiceListView.as_view(), name="global_search"),
 ]
